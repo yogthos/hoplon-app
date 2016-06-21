672aee8dfd2fcbb2a6de3d82da8d6328e2a5aa5a
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-06-21 16:30:04 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -13,8 +13,7 @@
                  [hiccup "1.0.5"]
                  [yogthos/config "0.8"]
                  [org.clojure/clojurescript "1.9.36"
-                  :scope "provided"]
-                 [secretary "1.2.3"]]
+                  :scope "provided"]]
 
   :plugins [[lein-environ "1.0.2"]
             [lein-cljsbuild "1.1.1"]
@@ -78,13 +77,13 @@
                    :dependencies [[ring/ring-mock "0.3.0"]
                                   [ring/ring-devel "1.4.0"]
                                   [prone "1.1.1"]
-                                  [lein-figwheel "0.5.4-SNAPSHOT"]
+                                  [figwheel-sidecar "0.5.3-2"]
                                   [org.clojure/tools.nrepl "0.2.12"]
                                   [com.cemerick/piggieback "0.2.2-SNAPSHOT"]
                                   [pjstadig/humane-test-output "0.8.0"]]
 
                    :source-paths ["env/dev/clj"]
-                   :plugins [[lein-figwheel "0.5.4-SNAPSHOT"]]
+                   :plugins [[lein-figwheel "0.5.3-2"]]
                    :injections [(require 'pjstadig.humane-test-output)
                                 (pjstadig.humane-test-output/activate!)]
 
